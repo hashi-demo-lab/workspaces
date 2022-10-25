5b901fa641be361d04a6eca2969b0008b7ddca38
Simon Lynch <44986346+srlynch1@users.noreply.github.com>
2022-10-25 12:17:02 +1100

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -1,1 +1,12 @@
-a
+terraform {
+  required_version = ">= 0.13.6, < 2.0"
+  cloud {
+    organization = "demo-lab-hashicorp"
+  }
+}
+
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
+}
